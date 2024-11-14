4e2970d92bd22058fee9bb5f6d06090093bfb16f
Vladimir Drugov <wolffdrugov@yandex.ru>
2024-11-14 19:06:45 +0300

    master:linking config files

--- links.sh
+++ links.sh
@@ -13,11 +13,28 @@ sudo ln -s $HOME/Desktop/personal/dotfiles/.desktop/obsidian.desktop $HOME/.loca
 
 sudo rm -rf /etc/nvim &> /dev/null
 sudo rm -rf ~/.config/nvim &> /dev/null
-sudo  ln -s $HOME/Desktop/personal/dotfiles/nvim /etc/nvim
 sudo  ln -s $HOME/Desktop/personal/dotfiles/nvim ~/.config/nvim
 
 
 sudo rm $HOME/.zshrc &> /dev/null
-sudo ln -s $HOME/Desktop/personal/dotfiles/oh-my-zsh/.zshrc $HOME/.zshrc
 sudo rm -rf $HOME/oh-my-zsh/ &> /dev/null
+sudo ln -s $HOME/Desktop/personal/dotfiles/oh-my-zsh/.zshrc $HOME/.zshrc
 sudo ln -s $HOME/Desktop/personal/dotfiles/oh-my-zsh/ $HOME/oh-my-zsh
+
+if [ ! -d $HOME/Pictures/personal ]; then
+    sudo mkdir -p $HOME/Pictures/personal
+fi
+
+sudo rm -rf ~/Pictures/personal/wallpaper
+sudo ln -s ~/Desktop/personal/dotfiles/wallpaper ~/Pictures/personal/wallpaper
+
+if [ ! -d $HOME/.config ]; then
+    sudo mkdir -p $HOME/.config
+fi
+sudo rm -rf ~/.config/picom
+sudo ln -s ~/Desktop/personal/dotfiles/picom ~/.config/picom
+
+sudo rm -f ~/.tmux.conf
+sudo rm -f ~/tmux.conf
+sudo ln -s ~/Desktop/personal/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+sudo ln -s ~/Desktop/personal/dotfiles/tmux/.tmux.conf ~/tmux.conf
--- runs/libs.sh
+++ runs/libs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 sudo apt -y update
-sudo apt -y install git fzf ripgrep pavucontrol xclip jq flameshot python3-pip i3 zsh picom tmux alacritty
+sudo apt -y install git fzf ripgrep pavucontrol xclip jq flameshot python3-pip i3 zsh picom tmux alacritty feh
+chsh -s $(which zsh)
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
